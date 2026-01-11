ce4fc73cbb835344b108175436e10a2407edecfc
jukim1234 <jukim1234@korea.com>
2026-01-11 07:46:52 +0000

    fix: update svelte config for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/kit/node'; // 이 부분을 수정합니다
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter() // Vercel이 이해할 수 있는 기본 어댑터 설정입니다
 	}
 };
 
